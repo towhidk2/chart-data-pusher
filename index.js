439fa1df23c56352c2766309efb1c445494db608
Towhid Khandaker <towhidk2@gmail.com>
2023-08-24 13:30:29 +0600

    updates

--- index.js
+++ index.js
@@ -1,145 +1,70 @@
 const fs = require('fs');
-const path = require('path');
+const axios = require('axios');
+const readline = require('readline');
 
 const output = [];
 
-const files = fs.readdirSync(__dirname).filter(file => path.extname(file) === '.csv');
+const outputFile = 'Data.js'; // Output JavaScript file name
 
-if (files.length === 1) {
-  const inputFile = files[0];
-  const outputFile = 'Data.js'; // Output JavaScript file name
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-  const readStream = fs.createReadStream(inputFile, { encoding: 'utf-8' });
+rl.question('Enter the stock symbol: ', (symbol) => {
+    rl.close();
 
-  let isFirstRow = true;
+    const start_time = Math.floor(new Date('2022-12-01T23:59:00').getTime() / 1000);
+    const end_time = Math.floor(new Date('2023-08-24T23:59:00').getTime() / 1000);
+    const interval = '1d';
 
-  readStream.on('data', (data) => {
-    const lines = data.split('\n');
-    for (const line of lines) {
-      if (isFirstRow) {
-        isFirstRow = false;
-        continue; // Skip the first row
-      }
+    const query_string = `https://query1.finance.yahoo.com/v7/finance/download/${symbol}?period1=${start_time}&period2=${end_time}&interval=${interval}&events=history&includeAdjustedClose=true`;
 
-      const values = line.split(',');
+    axios.get(query_string)
+        .then(response => {
+            const csvData = response.data;
 
-      if (values.length >= 5) {
-        const time = parseInt(values[0]);
-        const open = parseFloat(values[1]);
-        const high = parseFloat(values[2]);
-        const low = parseFloat(values[3]);
-        const close = parseFloat(values[4]);
+            const lines = csvData.split('\n');
+            for (let i = 1; i < lines.length; i++) {
+                const values = lines[i].split(',');
+                if (values.length >= 6) {
+                    const time = parseInt(new Date(values[0]).getTime() / 1000);
+                    const open = parseFloat(values[1]);
+                    const high = parseFloat(values[2]);
+                    const low = parseFloat(values[3]);
+                    const close = parseFloat(values[4]);
 
-        output.push({
-          time,
-          open,
-          high,
-          low,
-          close
+                    output.push({
+                        time,
+                        open,
+                        high,
+                        low,
+                        close
+                    });
+                }
+            }
+
+            const outputString = `const chartData = ${formatOutput(output)};\n\nexport default chartData;\n`;
+
+            fs.writeFileSync(outputFile, outputString, 'utf-8');
+            console.log(`Data has been written to ${outputFile}`);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
         });
-      }
-    }
-  });
+});
 
-  readStream.on('end', () => {
-    const outputString = `const data = ${formatOutput(output)};\n\nexport default data;\n`;
-
-    fs.writeFileSync(outputFile, outputString, 'utf-8');
-
-    console.log(`Data has been written to ${outputFile}`);
-  });
-
-  function formatOutput(data) {
+function formatOutput(data) {
     return '[' + data.map(entry => `{
-      time: ${entry.time},
-      open: ${entry.open},
-      high: ${entry.high},
-      low: ${entry.low},
-      close: ${entry.close}
+        time: ${entry.time},
+        open: ${entry.open},
+        high: ${entry.high},
+        low: ${entry.low},
+        close: ${entry.close}
     }`).join(',\n') + ']';
-  }
-} else {
-  console.log('Error: There should be only one CSV file in the current directory.');
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fs = require('fs');
-
-// const inputFile = 'nvda_1d.csv'; // Replace with your CSV file name
-// const outputFile = 'Data.js';  // Output JavaScript file name
-
-// const output = [];
-
-// const readStream = fs.createReadStream(inputFile, { encoding: 'utf-8' });
-
-// let isFirstRow = true;
-
-// readStream.on('data', (data) => {
-//   const lines = data.split('\n');
-//   for (const line of lines) {
-//     if (isFirstRow) {
-//       isFirstRow = false;
-//       continue; // Skip the first row
-//     }
-
-//     const values = line.split(',');
-
-//     if (values.length >= 5) {
-//       const time = parseInt(values[0]);
-//       const open = parseFloat(values[1]);
-//       const high = parseFloat(values[2]);
-//       const low = parseFloat(values[3]);
-//       const close = parseFloat(values[4]);
-
-//       output.push({
-//         time,
-//         open,
-//         high,
-//         low,
-//         close
-//       });
-//     }
-//   }
-// });
-
-// readStream.on('end', () => {
-//   const outputString = `const chartData = ${formatOutput(output)};\n\nexport default chartData;\n`;
-
-//   fs.writeFileSync(outputFile, outputString, 'utf-8');
-
-//   console.log(`Data has been written to ${outputFile}`);
-// });
-
-// function formatOutput(data) {
-//   return '[' + data.map(entry => `{
-//     time: ${entry.time},
-//     open: ${entry.open},
-//     high: ${entry.high},
-//     low: ${entry.low},
-//     close: ${entry.close}
-//   }`).join(',\n') + ']';
-// }
 
